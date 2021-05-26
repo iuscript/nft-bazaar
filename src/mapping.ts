f8ddc9a7b63738ec1064e2dc4d5fd8f9fb2f291f
iuscript <6593942@qq.com>
2021-05-26 11:26:10 +0800

    OrderList

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,6 +1,7 @@
-import { Address } from '@graphprotocol/graph-ts'
+import { Address, store } from '@graphprotocol/graph-ts'
 import { Transfer, NFTBazaar } from '../generated/NFTBazaar/NFTBazaar'
-import { User, Nft } from '../generated/schema'
+import { Bought, Offered, Offered__Params } from '../generated/NFTMarket/NFTMarket'
+import { User, Nft, Offer, Order } from '../generated/schema'
 
 export const ADDRESS_ZERO = '0x0000000000000000000000000000000000000000'
 export const NFTBazaar_ADDRSS = '0x0663b99715199d78850836Ba93dd479955E5105D'
@@ -30,4 +31,27 @@ export function handleTransfer(event: Transfer): void {
     nft.holder = to.toHexString()
     nft.save()
   }
+}
+
+export function handleOffered(event: Offered): void {
+  let offer = new Offer(event.params.tokenID.toHexString())
+  offer.seller = event.transaction.from
+  offer.price = event.params.price
+  offer.paymentToken = event.params.paymentToken
+  offer.createdAtTimestamp = event.block.timestamp
+  offer.createdAtBlockNumber = event.block.number
+  offer.save()
+}
+
+export function handleBought(event: Bought): void {
+  let order = new Order(event.params.tokenID.toHexString().concat('-').concat(event.block.timestamp.toString()))
+  order.seller = event.params.seller
+  order.buyers = event.params.buyers
+  order.tokenID = event.params.tokenID.toHexString()
+  order.price  = event.params.price
+  order.paymentToken = event.params.paymentToken
+  order.createdAtTimestamp = event.block.timestamp
+  order.createdAtBlockNumber = event.block.number
+  order.transactionHash = event.transaction.hash
+  order.save()
 }
