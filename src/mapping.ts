fd2a7e3ce43491c29454e0ba82be5ae8a961656c
iuscript <6593942@qq.com>
2021-06-03 17:39:15 +0800

    contract v2.1.0

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,7 +1,8 @@
-import { Address, BigInt, dataSource, store } from '@graphprotocol/graph-ts'
+import { Address, BigInt, store } from '@graphprotocol/graph-ts'
 import { Transfer, NFTBazaar as NFTBazaarContract } from '../generated/NFTBazaar/NFTBazaar'
 import { Offered, Bought, NoLongerForSale } from '../generated/NFTMarket/NFTMarket'
-import { User, Nft, Offer, Order, Market, DayData } from '../generated/schema'
+import { Offered as Offered_v2, BidEntered } from '../generated/NFTMarket_v2/NFTMarket_v2'
+import { User, Nft, Offer, Order, Market, DayData, Bid } from '../generated/schema'
 
 export const ADDRESS_ZERO = '0x0000000000000000000000000000000000000000'
 export const NFTBazaar_ADDRSS = '0x93e97BE3755EC8D54B464F310171c5DE51b1b461'
@@ -29,7 +30,9 @@ export function handleTransfer(event: Transfer): void {
     nft.tokenURI = tokenUri
     nft.createdAtTimestamp = event.block.timestamp
     nft.createdAtBlockNumber = event.block.number
+    nft.transactionHash = event.transaction.hash
     nft.holder = user.id
+    nft.creater = to
     nft.save()
   } else {
     let nft = Nft.load(tokenID.toHexString())
@@ -40,13 +43,43 @@ export function handleTransfer(event: Transfer): void {
 
 export function handleOffered(event: Offered): void {
   let offer = new Offer(event.params.tokenID.toHexString())
+  offer.isBid = false
   offer.seller = event.transaction.from
   offer.price = event.params.price
   offer.tokenID = offer.id
   offer.paymentToken = event.params.paymentToken
+  offer.endTime = BigInt.fromI32(0)
   offer.createdAtTimestamp = event.block.timestamp
   offer.createdAtBlockNumber = event.block.number
   offer.transactionHash = event.transaction.hash
+  offer.contract = event.address
+  offer.save()
+
+  let market = Market.load(NFTMarket_ADDRSS)
+  if (market === null) {
+    market = new Market(NFTMarket_ADDRSS)
+    market.txCount = BigInt.fromI32(0)
+    market.offersCount = BigInt.fromI32(1)
+    market.volumeETH = BigInt.fromI32(0)
+    market.volumeUSD = BigInt.fromI32(0)
+  } else {
+    market.offersCount = market.offersCount.plus(BigInt.fromI32(1))
+  }
+  market.save()
+}
+
+export function handleOffered_v2(event: Offered_v2): void {
+  let offer = new Offer(event.params.tokenID.toHexString())
+  offer.isBid = event.params.isBid
+  offer.seller = event.params.seller
+  offer.price = event.params.price
+  offer.tokenID = offer.id
+  offer.paymentToken = event.params.paymentToken
+  offer.endTime = event.params.endTime
+  offer.createdAtTimestamp = event.block.timestamp
+  offer.createdAtBlockNumber = event.block.number
+  offer.transactionHash = event.transaction.hash
+  offer.contract = event.address
   offer.save()
 
   let market = Market.load(NFTMarket_ADDRSS)
@@ -116,4 +149,15 @@ export function handleNoLongerForSale(event: NoLongerForSale): void {
   let market = Market.load(NFTMarket_ADDRSS)
   market.offersCount = market.offersCount.minus(BigInt.fromI32(1))
   market.save()
+}
+
+export function handleBidEntered(event: BidEntered): void {
+  let bid = new Bid(event.params.tokenID.toHexString().concat('-').concat(event.block.timestamp.toString()))
+  bid.tokenID = event.params.tokenID.toHexString()
+  bid.bidder = event.params.fromAddress
+  bid.value = event.params.value
+  bid.createdAtTimestamp = event.block.timestamp
+  bid.createdAtBlockNumber = event.block.number
+  bid.transactionHash = event.transaction.hash
+  bid.save()
 }
